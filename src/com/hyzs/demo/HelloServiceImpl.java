b0dfc746f9ad13ef6c6791d6fb611f2829216130
25809 <liyong@szhcf.com.cn>
2019-06-22 10:07:53 +0800

    <style>(<$HelloServiceImp>):<修改22222>

--- src/com/hyzs/demo/HelloServiceImpl.java
+++ src/com/hyzs/demo/HelloServiceImpl.java
@@ -6,6 +6,8 @@ package com.hyzs.demo;
 public class HelloServiceImpl implements HelloService{
     @Override
     public void sayHello(String name){
+
         System.out.println("hello"+name);
+        System.out.println("222222");
     }
 }
